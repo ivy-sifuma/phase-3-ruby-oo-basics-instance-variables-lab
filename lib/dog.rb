ae2b9db4f44d709f0a1c14c7077d5698876dfd7d
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-22 14:36:45 +0300

    Completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
 
+    def name=(dogs_name)  ##setter method
+        @this_dogs_name = dogs_name
+      end
+      def name  ##getter method
+        @this_dogs_name
+      end
+
 end
+
+fluffy = Dog.new
+fluffy.name = "Fluffy"
+
+puts fluffy.name 
